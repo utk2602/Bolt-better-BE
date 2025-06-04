524357232333ceb9343797fc5a1c4f98605f7252
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-06-04 18:50:22 +0530

    FInal BE

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -1,7 +1,47 @@
 require("dotenv").config()
 import Anthropic from '@anthropic-ai/sdk';
+import  express from 'express';
+import { basePrompt as nodeBasePrompt } from './defaults/node';
+import { basePrompt as reactBasePrompt } from './defaults/react';
+import fs from 'fs';
+import { ContentBlock,TextBlock } from '@anthropic-ai/sdk/resources/messages';
 
 const anthropic = new Anthropic();
+const app = express();
+app.use(express.json()); 
+app.post("/template", async (req, res) => {
+    const prompt = req.body.prompt;
+    
+    const response = await anthropic.messages.create({
+        messages: [{
+            role: 'user', content: prompt
+        }],
+        model: 'claude-3-5-sonnet-20241022',
+        max_tokens: 200,
+        system: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra"
+    })
+
+    const answer = (response.content[0] as TextBlock).text; 
+    if (answer == "react") {
+        res.json({
+            prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [reactBasePrompt]
+        })
+        return;
+    }
+
+    if (answer === "node") {
+        res.json({
+            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [nodeBasePrompt]
+        })
+        return;
+    }
+
+    res.status(403).json({message: "You cant access this"})
+    return;
+
+})
 
 async function main() {   
   const msg = await anthropic.messages.create({
