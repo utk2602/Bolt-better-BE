be540e21f271fab808488d1551d68eab3f601fa4
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-07-01 01:54:04 +0530

    FE setup done

---
+++ FE/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p>Start prompting (or editing) to see magic happen :)</p>
+    </div>
+  );
+}
