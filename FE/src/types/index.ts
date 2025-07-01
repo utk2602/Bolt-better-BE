f2d5e15803267a08fd115e360ede3c81fd5cdfec
utkarsh <166751207+utk2602@users.noreply.github.com>
2025-07-01 12:25:21 +0530

    Steps and types sorted

--- FE/src/types/index.ts
+++ FE/src/types/index.ts
@@ -10,3 +10,11 @@ export interface FIleViewerProps{
     file:FileItem|null;
     onClose:()=> void;
 }
+export interface Step{
+    id:number;
+    title:string;
+    description:string;
+    status: 'pending' | 'in-progress' | 'completed';
+    code?: string;
+    path?: string;
+}
